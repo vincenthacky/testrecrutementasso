68df0f8d95e0c67f59959fb2e0965a5fe0d122a6
vincenthacky <assonchovincentde12@gmail.com>
2025-12-16 22:13:10 +0000

    refactorin 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { getDatabaseConfig } from './config/database.config';
@@ -16,7 +14,7 @@ import { getDatabaseConfig } from './config/database.config';
     UsersModule,
     TransactionsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Bienvenue sur l’API d’Asso – le futur développeur retenu pour le poste';
-  }
-}
